cd8ca5dd6016c4fd1b810de77ddd8bf1b61b9fe8
Ckorine <keli.dara-ahato@stud.uni-due.de>
2017-05-09 11:48:36 +0200

    Ich habe Aufgabe 2 a,b,c

--- Startprojekt u1/u1/src/main/java/fpt/model/Song.java
+++ Startprojekt u1/u1/src/main/java/fpt/model/Song.java
@@ -1,8 +1,84 @@
 package fpt.model;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+
+import java.util.ArrayList;
+
 /**
  * Created by corin on 05.05.2017.
  */
-public class Song {
+public class Song extends SimpleStringProperty implements fpt.interfaces.Song{
 
+    private SimpleStringProperty path = new SimpleStringProperty();
+    private SimpleStringProperty titel = new SimpleStringProperty();
+    private SimpleStringProperty album = new SimpleStringProperty();
+    private SimpleStringProperty interpret = new SimpleStringProperty();
+    private long id;
+
+
+
+    @Override
+    public void setAlbum(String album) {
+       this.album.set(album);
+    }
+
+    @Override
+    public String getInterpret() {
+        return interpret.get();
+    }
+
+    @Override
+    public void setInterpret(String interpret) {
+        this.interpret.set(interpret);
+
+    }
+
+    @Override
+    public String getPath() {
+
+        return path.get();
+    }
+
+    @Override
+    public void setPath(String path) {
+        this.path.set(path);
+
+    }
+
+    @Override
+    public String getTitle() {
+
+        return titel.get();
+    }
+
+    @Override
+    public void setTitle(String title) {
+         this.titel.set(title);
+    }
+
+    @Override
+    public long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public ObservableValue<String> pathProperty() {
+        return path;
+    }
+
+    @Override
+    public ObservableValue<String> albumProperty() {
+        return album;
+    }
+
+    @Override
+    public ObservableValue<String> interpretProperty() {
+        return interpret;
+    }
 }
--- Startprojekt u1/u1/src/main/java/fpt/model/SongList.java
+++ Startprojekt u1/u1/src/main/java/fpt/model/SongList.java
@@ -1,7 +1,79 @@
 package fpt.model;
 
+import fpt.interfaces.*;
+import fpt.interfaces.Song;
+import javafx.collections.ModifiableObservableListBase;
+
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
  * Created by corin on 05.05.2017.
  */
-public class SongList {
+public class SongList extends ModifiableObservableListBase<Song> implements fpt.interfaces.SongList {
+    @Override
+    public boolean addSong(Song s) throws RemoteException {
+        return false;
+    }
+
+    @Override
+    public boolean deleteSong(Song s) throws RemoteException {
+        return false;
+    }
+
+    @Override
+    public void setList(ArrayList<Song> s) throws RemoteException {
+
+    }
+
+    @Override
+    public ArrayList<Song> getList() throws RemoteException {
+        return null;
+    }
+
+    @Override
+    public void deleteAllSongs() throws RemoteException {
+
+    }
+
+    @Override
+    public int sizeOfList() throws RemoteException {
+        return 0;
+    }
+
+    @Override
+    public Song findSongByPath(String name) throws RemoteException {
+        return null;
+    }
+
+    @Override
+    public Iterator<Song> iterator() {
+        return null;
+    }
+
+    @Override
+    public Song get(int index) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    protected void doAdd(int index, Song element) {
+
+    }
+
+    @Override
+    protected Song doSet(int index, Song element) {
+        return null;
+    }
+
+    @Override
+    protected Song doRemove(int index) {
+        return null;
+    }
 }
