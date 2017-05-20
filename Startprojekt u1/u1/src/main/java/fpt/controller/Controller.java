96ab250fd01de9b6faf180469b7da15bf7876b9c
Ckorine <keli.dara-ahato@stud.uni-due.de>
2017-05-21 01:24:33 +0200

    play stop   button pause  und deren funktionalität

--- Startprojekt u1/u1/src/main/java/fpt/controller/Controller.java
+++ Startprojekt u1/u1/src/main/java/fpt/controller/Controller.java
@@ -1,36 +1,35 @@
 package fpt.controller;
 
-
-import fpt.interfaces.ButtonAction;
 import fpt.interfaces.Song;
 import fpt.model.Model;
 import fpt.model.SongList;
 import fpt.view.View;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
+import java.io.File;
 import java.rmi.RemoteException;
 
 
 /**
  * Created by corin on 09.05.2017.
  */
-public class Controller implements ButtonAction {
+public class Controller {
     private View view;
     private Model model;
-    private String nichts ;
+    private Media media ;
+    MediaPlayer mediaPlayer ;
 
     public Controller() {
 
     }
 
-    public void link (Model model,View view){
+    public void link(Model model, View view) {
         this.model = model;
         this.view = view;
 
-        view.link(this);
-
         view.fillSongList(model.getAllSongs());
         view.fillPlayList(model.getPlaylist());
-
 
         view.getAddToPlayButton().setOnAction(event -> {
             Song s = view.getSongList().getSelectionModel().getSelectedItem();
@@ -43,10 +42,11 @@ public class Controller implements ButtonAction {
                 e.printStackTrace();
             }
         });
+
         view.getAddall().setOnMouseClicked(event -> {
             try {
                 SongList sl = model.getAllSongs();
-                for(Song s:sl){
+                for (Song s : sl) {
                     model.getPlaylist().addSong(s);
                 }
                 view.fillPlayList(null);
@@ -56,6 +56,7 @@ public class Controller implements ButtonAction {
                 e.printStackTrace();
             }
         });
+
         view.getRemoveFromPLay().setOnMousePressed(event -> {
             Song s = view.getPlayList().getSelectionModel().getSelectedItem();
             try {
@@ -68,20 +69,43 @@ public class Controller implements ButtonAction {
             }
         });
 
+        view.getPLay().setOnAction(e ->
+        {
 
+                if (mediaPlayer != null  ) {
+                    if (!mediaPlayer.isMute()) {
+                        mediaPlayer.stop();
+                    }
+                }
+
+            Song s = view.getPlayList().getSelectionModel().getSelectedItem();
+            String s1 = s.getPath();
+            media = new Media(new File(s1).toURI().toString());
+            mediaPlayer = new MediaPlayer(media);
+            mediaPlayer.play();
+
+
+
+        });
+
+        view.getStop().setOnAction(e ->
+        {
+            if(mediaPlayer!= null){
+                if(!mediaPlayer.isMute())
+                    mediaPlayer.stop();
+            }
+
+        });
+        view.getPause().setOnAction(e ->
+        {
+            if(mediaPlayer!= null){
+                if(!mediaPlayer.isMute())
+                    mediaPlayer.pause();
+            }
+
+        });
 
     }
-    @Override
-    public void play() {
-
-    }
-    @Override
-    public void stop() {
-
-    }
-
-
-
 }
 
 
--- Startprojekt u1/u1/src/main/java/fpt/model/Model.java
+++ Startprojekt u1/u1/src/main/java/fpt/model/Model.java
@@ -1,7 +1,12 @@
 package fpt.model;
 
 import java.io.File;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
 import fpt.interfaces.Song;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
 /**
  * Created by corin on 09.05.2017.
@@ -10,12 +15,40 @@ import fpt.interfaces.Song;
 public class Model {
     private SongList allSongs = new SongList();
     private SongList playlist = new SongList();
+    private ArrayList<Song> list = new ArrayList();
+    public ArrayList<Song> getList(){
+        return list;
+    }
 
     public Model() {
-        allSongs.file();
 
+        try {
+            allSongs.file();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    File lieder = new File("C:\\Users\\corin\\Documents\\GitHub\\fpt-ss17\\Startprojekt u1\\u1\\src\\res\\files");
+    File [] listOfLieder = lieder.listFiles();
+
+
+    public SongList file() throws RemoteException {
+        SongList sl = new SongList();
+        for (File f : listOfLieder) {
+            if (f.getAbsolutePath().endsWith(".mp3")) {
+                System.out.println(f.getName());
+                list.add(new fpt.model.Song(f.getName(), f.getAbsolutePath()));
+                sl.setList(getList());
+
+            }
 
         }
+        return sl;
+    }
+    public void play() {
+
+    }
     public SongList getAllSongs() {
         return allSongs;
     }
